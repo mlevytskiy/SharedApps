4c6824dd397e62e32c4e9dcf684a91f69af4bdbf
Maksym Levytskyi <maksym.levytskyi@mantu.im>
2016-11-29 22:36:36 +0200

    start working with page follow/unfollow people when nobody from friends use this app.

--- app/src/main/java/wumf/com/sharedapps/adapter/FollowUnfollowPeopleAdapter.java
+++ app/src/main/java/wumf/com/sharedapps/adapter/FollowUnfollowPeopleAdapter.java
@@ -12,9 +12,12 @@ import wumf.com.sharedapps.R;
 
 public class FollowUnfollowPeopleAdapter extends BaseAdapter {
 
+    private static final int VIEW_TYPE_NOBODY = 0;
+    private static final int VIEW_TYPE_NO_PEOPLE_NO_BAGS = 1;
+
     @Override
     public int getCount() {
-        return 1;
+        return 2;
     }
 
     @Override
@@ -29,18 +32,28 @@ public class FollowUnfollowPeopleAdapter extends BaseAdapter {
 
     @Override
     public int getViewTypeCount() {
-        return 1;
+        return 2;
     }
 
     @Override
     public int getItemViewType(int position) {
-        return 0;
+        if (position == 0) {
+            return VIEW_TYPE_NOBODY;
+        } else {
+            return VIEW_TYPE_NO_PEOPLE_NO_BAGS;
+        }
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        if (view == null) {
-            view = View.inflate(viewGroup.getContext(), R.layout.item_nobody_use_this_app, null);
+        if (getItemViewType(i) == VIEW_TYPE_NOBODY) {
+            if (view == null) {
+                view = View.inflate(viewGroup.getContext(), R.layout.item_nobody_use_this_app, null);
+            }
+        } else {
+            if (view == null) {
+                view = View.inflate(viewGroup.getContext(), R.layout.item_no_people_no_bags, null);
+            }
         }
         return view;
     }
